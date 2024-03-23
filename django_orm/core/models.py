9ea5c81fc93a8e9c72f8a80d5eaec2951b15821a
soaqa <soaqa57@gmail.com>
2024-03-23 16:57:28 +0300

    Django ORM Example

--- django_orm/core/models.py
+++ django_orm/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    id = models.BigIntegerField(primary_key=True, auto_created=True)
+    id = models.BigAutoField(primary_key=True)
     username = models.CharField(max_length=128, unique=True)
     password = models.CharField(max_length=256)
     email = models.EmailField()
@@ -11,21 +11,21 @@ class User(models.Model):
 
 
 class UserAddress(models.Model):
-    id = models.BigIntegerField(primary_key=True, auto_created=True)
+    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.ForeignKey("Address", on_delete=models.CASCADE)
 
 
 class Address(models.Model):
-    id = models.BigIntegerField(primary_key=True, auto_created=True)
+    id = models.BigAutoField(primary_key=True)
     country = models.CharField(max_length=128)
     city = models.CharField(max_length=128)
     street = models.CharField(max_length=128)
     house_number = models.PositiveIntegerField()
-    users = models.ManyToManyField(User, through="UserAddress")
+    users = models.ManyToManyField(User, through="UserAddress", related_name="addresses")
 
 
 class Invoice(models.Model):
-    id = models.BigIntegerField(primary_key=True, auto_created=True)
+    id = models.BigAutoField(primary_key=True)
     cost = models.DecimalField(max_digits=12, decimal_places=2)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="invoices")
---
+++ django_orm/core/test/test_database.py
@@ -0,0 +1,99 @@
+from django.contrib.auth.hashers import make_password
+from django.db import connection
+from django.test import TestCase
+from django.test.utils import CaptureQueriesContext
+
+from core.models import User, Address
+
+
+class DatabaseTests(TestCase):
+    def test_create_and_get_user(self):
+        user = User.objects.create(
+            username="test",
+            password=make_password("mypass"),
+            email="test@localhost",
+            name="Oleg",
+        )
+
+        assert User.objects.get(username=user.username)
+
+    def test_create_multiple_users(self):
+        users = []
+
+        for i in range(10):
+            users.append(
+                User(
+                    username=f"test{i}",
+                    password=make_password("mypass"),
+                    email=f"test{i}@localhost",
+                    name="Oleg",
+                )
+            )
+
+        User.objects.bulk_create(users)
+
+        assert User.objects.count() >= 10
+
+        print(User.objects.all())
+
+    def test_create_related(self):
+        with CaptureQueriesContext(connection) as ctx:
+            user = User.objects.create(
+                username="test",
+                password=make_password("mypass"),
+                email="test@localhost",
+                name="Oleg",
+            )
+
+            address = Address.objects.create(
+                country="test",
+                city="test",
+                street="test",
+                house_number=1,
+            )
+
+            address.users.add(user)  # NoQa
+
+            # user.addresses.add(address)
+
+            assert address.users.exists()
+            print(ctx.captured_queries)
+
+    def test_delete(self):
+        user = User.objects.create(
+            username="test",
+            password=make_password("mypass"),
+            email="test@localhost",
+            name="Oleg",
+        )
+
+        assert User.objects.get(username=user.username)
+
+        user.delete()
+
+        assert not User.objects.filter(username=user.username).exists()
+
+    def test_delete_related(self):
+        user = User.objects.create(
+            username="test",
+            password=make_password("mypass"),
+            email="test@localhost",
+            name="Oleg",
+        )
+
+        address = Address.objects.create(
+            country="test",
+            city="test",
+            street="test",
+            house_number=1,
+        )
+
+        address.users.add(user)  # NoQa
+
+        assert address.users.exists()
+
+        address.users.remove(user)
+        # address.users.clear()
+        # address.users.set(users)
+
+        assert not address.users.exists()
